fc98d4bed1354fb03532a396123dd3a83fdffa61
beoba <beoba@402a00b8-c300-0410-8674-aac35dbd710b>
2005-11-20 22:15:41 +0000

    added stubs for post classes

---
+++ trunk/BusinessLogic/Post/PostDataAccess.php
@@ -0,0 +1,62 @@
+<?php
+
+class BusinessLogic_Post_PostSecurity
+{
+//Helper class which interacts with the Data Access layer. This class is also responsible for converting data into a View structure.
+
+	public function ProcessNewPost()
+	{
+		//Inserts data into the Posts table.
+		//TODO
+	}
+
+	public function EditPost()
+	{
+		//Returns an EditPostView with data from the Posts table.
+		//TODO
+	}
+
+	public function ProcessEditPost()
+	{
+		//Updates the Posts table with the new data.
+		//TODO
+	}
+
+	public function DeletePost()
+	{
+		//Returns a DeletePostView with data from the Posts table.
+		//TODO
+	}
+
+	public function ProcessDeletePost()
+	{
+		//Updates the Posts table with the new data.
+		//TODO
+	}
+
+	public function ViewPost()
+	{
+		//Returns a ViewPostCollectionView with data from the Posts table.
+		//TODO
+	}
+
+	public function ViewPostByRecent()
+	{
+		//Returns a ViewPostCollectionView with data from the Posts table.
+		//TODO
+	}
+
+	public function ViewPostByDay()
+	{
+		//Returns a ViewPostCollectionView with data from the Posts table.
+		//TODO
+	}
+
+	public function ViewPostByMonth()
+	{
+		//Returns a ViewPostCollectionView with data from the Posts table.
+		//TODO
+	}
+}
+
+?>
---
+++ trunk/BusinessLogic/Post/PostSecurity.php
@@ -0,0 +1,68 @@
+<?php
+
+class BusinessLogic_Post_PostSecurity
+{
+//Helper class which determines whether a user can use a certain function.
+
+	public function NewPost()
+	{
+		//Returns true if the user has privilege {Author, Editor, Owner}. Otherwise, false.
+		//TODO
+	}
+
+	public function ProcessNewPost()
+	{
+		//Returns true if the user has privilege {Author, Editor, Owner}. Otherwise, false.
+		//TODO
+	}
+
+	public function EditPost()
+	{
+		//Returns true if the user has privilege {Editor, Owner}. Returns true if the user has privilege Author and is the creator of the post. Otherwise, false.
+		//TODO
+	}
+
+	public function ProcessEditPost()
+	{
+		//Returns true if the user has privilege {Editor, Owner}. Returns true if the user has privilege Author and is the creator of the post. Otherwise, false.
+		//TODO
+	}
+
+	public function DeletePost()
+	{
+		//Returns true if the user has privilege {Editor, Owner}. Returns true if the user has privilege Author and is the creator of the post. Otherwise, false.
+		//TODO
+	}
+
+	public function ProcessDeletePost()
+	{
+		//Returns true if the user has privilege {Editor, Owner}. Returns true if the user has privilege Author and is the creator of the post. Otherwise, false.
+		//TODO
+	}
+
+	public function ViewPost()
+	{
+		//Returns the privilege level of the user.
+		//TODO
+	}
+
+	public function ViewPostByRecent()
+	{
+		//Returns the privilege level of the user.
+		//TODO
+	}
+
+	public function ViewPostByDay()
+	{
+		//Returns the privilege level of the user.
+		//TODO
+	}
+
+	public function ViewPostByMonth()
+	{
+		//Returns the privilege level of the user.
+		//TODO
+	}
+}
+
+?>
