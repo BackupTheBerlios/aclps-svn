7b9b5e065a36164d81d29926d209231f6618b560
dekken <dekken@402a00b8-c300-0410-8674-aac35dbd710b>
2005-11-29 04:17:32 +0000

    Cleaned up index.php.
    -JAC

--- trunk/index.php
+++ trunk/index.php
@@ -1,13 +1,7 @@
 <?php
 
-print_r($_SESSION);
-
 session_start();
 
-//CHANGE '\' to '/'
-
-//If you're getting weird file-not-found errors for a file that was recently moved in the code,
-//you need to clear the cs411.beoba.net cookie in your browser. - Nick
 function __autoload($className)
 {
     $path = str_replace('_', '/', $className);
