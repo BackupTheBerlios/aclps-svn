d5832dafaf3f35b9ae1ba7023e6069b19849cba0
stots <stots@402a00b8-c300-0410-8674-aac35dbd710b>
2005-11-30 21:06:46 +0000

    created vieweditblogimages, coded edit blog images process edit blog images and processeditabout

---
+++ trunk/Presentation/View/EditBlogImagesView.php
@@ -0,0 +1,40 @@
+<?php
+
+class Presentation_View_EditBlogImagesView extends Presentation_View_View
+{
+	private $headerImage;
+	private $footerImage;
+	private $blogID;
+
+	public function __construct($headerImage,$footerImage,$blogID)
+	{
+		
+		$this->headerImage = $headerImage;
+		$this->footerImage = $footerImage;
+		$this->blogID = $blogID;
+	}
+
+    public function Display()
+    {
+		$form = '<form method="post" action="index.php?Action=EditBlogImages&blogID=' . $this->blogID . '">'
+            . '<fieldset>'
+            . '<legend>&nbsp;Edit header and footer images</legend>'
+            . '<p>'
+            . '<center>Edit the field below:</center>'
+            . '</p>'
+            . '<input type="text" name="headerImage" value="' . $this->headerImage . '">'
+            . '<br />'
+			. '<input type="text" name="footerImage" value="' . $this->footerImage . '">'
+            . '<br />'
+            . '<input type="submit" class="submit-login" value="Submit">'
+            . '</fieldset>'
+            . '</form>';
+
+			return $form;
+ 
+    }
+
+	
+}
+
+?>
---
+++ trunk/Presentation/View/ViewEditBlogImagesView.php
@@ -0,0 +1,23 @@
+<?php
+
+class Presentation_View_ViewEditBlogImagesView extends Presentation_View_View
+{
+	private $headerImage;
+	private $footerImage;
+
+	public function __construct($headerImage,$footerImage)
+	{
+		
+		$this->headerImage = $headerImage;
+		$this->footerImage = $footerImage;
+	}
+
+    public function Display()
+    {
+		$images = array($this->headerImage,$this->footerImage);
+		return $images;
+ 
+    }
+}
+
+?>
