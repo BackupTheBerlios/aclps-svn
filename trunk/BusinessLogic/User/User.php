69aac319eab4d9f5d762bcf5fbd7ab99d75722d4
dekken <dekken@402a00b8-c300-0410-8674-aac35dbd710b>
2005-12-01 02:19:18 +0000

    What ya know, you can signin and register now!

--- trunk/BusinessLogic/User/User.php
+++ trunk/BusinessLogic/User/User.php
@@ -14,10 +14,10 @@ class BusinessLogic_User_User
     {
     	if (!isset($_SESSION['BusinessLogic_User_User']))
 	    {
-	        $_SESSION['BusinessLogic_User_User'] = new BusinessLogic_User_User();
+	        $_SESSION['BusinessLogic_User_User'] = serialize(new BusinessLogic_User_User());
    	    }
    	    
-	    return $_SESSION['BusinessLogic_User_User'];
+	    return unserialize($_SESSION['BusinessLogic_User_User']);
     }
 
     public function HandleRequest()
@@ -61,7 +61,7 @@ class BusinessLogic_User_User
             case 'ProcessSignIn':
                 if ($_POST['username'] != '' and $_POST['password'] != '')
                 {
-                    $this->ProcessSignIn($_POST['username'], $_POST['password']);
+                    return $this->ProcessSignIn($_POST['username'], $_POST['password']);
                 }
                 else
                 {
@@ -114,7 +114,7 @@ class BusinessLogic_User_User
 
     public function ProcessRegister($username, $email, $password)
     {
-        $query = 'select * from [0] where Username=[1]';
+        $query = "select * from [0] where Username='[1]'";
         $arguments = array('Users', $username);
         
         $DataAccess = DataAccess_DataAccessFactory::GetInstance();
@@ -123,7 +123,7 @@ class BusinessLogic_User_User
         //there is no matching record in the DB
         if (count($result) == 0)
         {
-            $query = 'insert into [0] (Username, Email, Password) VALUES ([1],[2],sha1([3]))';
+            $query = "insert into [0] (Username, Email, Password) VALUES ('[1]','[2]',sha1('[3]'))";
             $arguments = array('Users', $username, $email, $password);
             $result = $DataAccess->Insert($query, $arguments);
             
@@ -151,25 +151,28 @@ class BusinessLogic_User_User
 
     public function ProcessSignIn($username, $password)
     {
-        $query = 'select * from [0] where Username=[1] and Password=sha1([2])';
+        $query = "select UserID,Username,Email from [0] where Username='[1]' and Password=sha1('[2]')";
         $arguments = array('Users', $username, $password);
 
         $DataAccess = DataAccess_DataAccessFactory::GetInstance();
         $result = $DataAccess->Select($query, $arguments);
+        
+
 
         //there is a matching record in the DB
         if (count($result) > 0)
         {
-            $this->userInfo = $result;
+            //DataAccess wraps the rows into a new table. We need to get the first row, i.e. [0]
+            $this->userInfo = $result[0];
 
             //Need to fill in permissions table
             $query = 'select BlogID, Auth from [0] where UserID=[1]';
-            $arguments = array('User_Auth', $result['UserID']);
+            $arguments = array('User_Auth', $result[0]['UserID']);
             $result = $DataAccess->Select($query, $arguments);
             $this->permissions = $result;
 
             //Need to store all this information
-            $_SESSION['BusinessLogic_User_User'] = $this;
+            $_SESSION['BusinessLogic_User_User'] = serialize($this);
 
             //We need to redirect since we cannot directly change the $_GET['Action']
             $path = $_SERVER['DIRECTORY_ROOT'] . 'index.php?Action=ViewDashboard&blogID=1';
@@ -250,12 +253,27 @@ class BusinessLogic_User_User
     
     public function GetTopBar()
     {
+        print_r($this->userInfo);
+        print '<br />';
         return new Presentation_View_ViewTopBarView($this->CheckSignedIn());
     }
     
-    public static function ConvertUIDToName($userID)
+    public static function ConvertUserIDToName($userID)
     {
-      return 'WALRUS';
+        $query = "select Username from [0] where UserID=[1]";
+        $arguments = array('Users', $userID);
+
+        $DataAccess = DataAccess_DataAccessFactory::GetInstance();
+        $result = $DataAccess->Select($query, $arguments);
+        
+        if (isset($result[0]['Username']))
+        {
+          return $result[0]['Username'];
+        }
+        else
+        {
+          throw new Exception('Username does not exist for specified UserID');
+        }
     }
 }
 
