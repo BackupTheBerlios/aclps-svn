b11ee4b7f9ab4160e795f14337fdc32a38bef5a9
stots <stots@402a00b8-c300-0410-8674-aac35dbd710b>
2005-12-02 20:43:10 +0000

    finished editlinks and processedit links and created the corresponding views

---
+++ trunk/Presentation/View/EditLinksView.php
@@ -0,0 +1,46 @@
+<?php
+
+class Presentation_View_EditLinksView extends Presentation_View_View
+{
+	private $urls;	//array
+	private $titles; //array
+	private $blogID;
+
+	public function __construct($blogID,$urls,$titles)
+	{
+		
+		$this->urls = $urls;
+		$this->blogID = $blogID;
+	}
+
+    public function Display()
+    {
+		$count = $count($urls);	//this is the number of textboxes needed...
+
+		$form = '<form method="post" action="index.php?Action=EditLinks&blogID=' . $this->blogID . '">'
+            	. '<fieldset>'
+            	. '<legend>&nbsp;Edit Links</legend>'
+            	. '<p>'
+            	. '<center>Edit the links below:</center>'
+           		. '</p>'
+
+					
+		for($i = 0; $i < $count; ++$i;)
+		{
+            
+			$form = . '<input type="text" name="Url" value="' . $this->urls[i] . '">'
+            		. '<br />'
+					. '<input type="text" name="Title" value="' . $this->titles[i] . '">'
+            		. '<br />'
+ 
+        }
+		   $form = . '<input type="submit" class="submit-login" value="Submit">'
+            . '</fieldset>'
+			. '</form>';
+
+		return $form;
+ 
+    }	
+}
+
+?>
---
+++ trunk/Presentation/View/ViewEditLinksView.php
@@ -0,0 +1,25 @@
+<?php
+
+class Presentation_View_ViewEditLinksView extends Presentation_View_View
+{
+	private $urls;
+	private $titles;
+
+	public function __construct($urls,$titles)
+	{
+		
+		$this->urls = $urls;
+		$this->titles = $titles;
+	}
+		
+
+
+    public function Display()
+    {
+		$result =array($this->urls,$this->titles);
+		return $result;
+ 
+    }
+}
+
+?>
