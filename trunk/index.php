a7a26f15fe0a2496e0bb08542e23b72f0f7289d2
beoba <beoba@402a00b8-c300-0410-8674-aac35dbd710b>
2005-12-05 17:47:36 +0000

    shortening number of lines not in classes, adding newcomment img

--- trunk/index.php
+++ trunk/index.php
@@ -1,15 +1,10 @@
 <?php
-
 session_start();
-
 function __autoload($className)
 {
     $path = str_replace('_', '/', $className);
     require_once("$path.php");
 }
-
 $aFrontController = new Presentation_FrontController();
-
 $aFrontController->HandleRequest();
-
 ?>
