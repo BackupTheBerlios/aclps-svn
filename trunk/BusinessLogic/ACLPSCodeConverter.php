18046ae66ffc5bc634f4db3e33f70488b0ce4aa4
beoba <beoba@402a00b8-c300-0410-8674-aac35dbd710b>
2005-12-06 05:46:15 +0000

    apparently rss doesnt like html tags, theyre now stripped from the view. also, code converter now intelligently adds http to images and links that needed, and doesnt to those that dont

--- trunk/BusinessLogic/ACLPSCodeConverter.php
+++ trunk/BusinessLogic/ACLPSCodeConverter.php
@@ -13,7 +13,7 @@ class BusinessLogic_ACLPSCodeConverter
                                                 '[/title]'      => '</h1>',
                                                 '[subtitle]'    => '<h2>',
                                                 '[/subtitle]'   => '</h2>');
-                                                
+    
     public static function NewLineToBreak($content)
     {
         return str_replace("\n", '<br />', $content);
@@ -30,48 +30,62 @@ class BusinessLogic_ACLPSCodeConverter
         //[img]URL[/img] => <img src=URL>
         while(strstr($content, '[img]'))
         {
-        	$sub = strstr($content, '[img]');
-        	$offset = strpos($sub, '[/img]');
-        	if ($offset)
-        	{
-        		$URL = substr($sub, 5, $offset - 5);
-        		$content = str_replace('[img]' . $URL . '[/img]', '<img src="http://' . $URL . '">' , $content);
-        	}
-        	else
-        	{
-        		$content = str_replace('[img]', '', $content);
-        	}
+            $sub = strstr($content, '[img]');
+            $offset = strpos($sub, '[/img]');
+            if ($offset)
+            {
+                $URL = substr($sub, 5, $offset - 5);
+                if (strstr($URL,"://"))
+                {
+                    $content = str_replace('[img]' . $URL . '[/img]', '<img src="' . $URL . '">' , $content);
+                }
+                else
+                {
+                    $content = str_replace('[img]' . $URL . '[/img]', '<img src="http://' . $URL . '">' , $content);
+                }
+            }
+            else
+            {
+                $content = str_replace('[img]', '', $content);
+            }
         }
         
-        //[link=URL]name[/link] => <a href=URL>name</a>
+        //[link=URL]name[/link] => <a href="URL">name</a>
         while(strstr($content, '[link='))
         {
-        	$sub = strstr($content, '[link=');
-        	$offset = strpos($sub, ']');
-
-        	if ($offset)
-        	{
-        		$URL = substr($sub, 6, $offset - 6);
-        		$length = strlen('link=' . $URL . ']') + 1;
-
-            	$offset = strpos($sub, '[/link]');
-
-            	if ($offset)
-            	{
+            $sub = strstr($content, '[link=');
+            $offset = strpos($sub, ']');
+            
+            if ($offset)
+            {
+                $URL = substr($sub, 6, $offset - 6);
+                $length = strlen('link=' . $URL . ']') + 1;
+                
+                $offset = strpos($sub, '[/link]');
+                
+                if ($offset)
+                {
                     $name = substr($sub, $length, $offset - $length);
                     $needle = '[link=' . $URL . ']' . $name . '[/link]';
-                    $new_needle = '<a href=http://' . $URL . '>' . $name . '</a>';
+                    if (strstr($URL,"://"))
+                    {
+                        $new_needle = '<a href="'.$URL.'">'.$name.'</a>';
+                    }
+                    else
+                    {
+                        $new_needle = '<a href="http://' . $URL . '">' . $name . '</a>';
+                    }
                     $content = str_replace($needle, $new_needle, $content);
                 }
-        		else
-        		{
+                else
+                {
                     $content = str_replace('[link=' . $URL .']', '', $content);
                 }
-        	}
-        	else
-        	{
-        		$content = str_replace('[link=', '', $content);
-        	}
+            }
+            else
+            {
+                $content = str_replace('[link=', '', $content);
+            }
         }
         
         return $content;
